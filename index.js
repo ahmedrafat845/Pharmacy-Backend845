419b87ce57accf5c802d39b52b5a5a7e2a79dd52
ahmedrafat845 <za399012@gmail.com>
2024-09-30 15:54:44 +0300

    second

--- index.js
+++ index.js
@@ -19,24 +19,7 @@ app.use('/users',userRouter)
 app.use('/products',productRouter)
 app.use('/carts',cartRouter)
 app.use('/wishlist',wishlistRouter)
-app.get('/payment', (req, res) => res.send('Hello World!'));
+// app.get('/payment', (req, res) => res.send('Hello World!'));
 app.use('/payment', paymentRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-// {
-//     "userId": "66f851ed780f05324fe2e752",  // replace with a valid ObjectId
-//     "amount": 100.00, {
-//     "email": "eman23121@gmail.com",
-//     "password": "NewSecurePassword123"
-// }
-
-//     "items": [
-//         {
-//             "productId": "PRODUCT_ID_HERE",  // Optional
-//             "quantity": 1,
-//             "price": 100.00
-//         }
-//     ],
-//     "paymentMethod": "credit-card"
-// }
