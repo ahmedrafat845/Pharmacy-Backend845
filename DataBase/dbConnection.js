f0bf8bac979587b6b8b07a06cb1e77c83fe241ce
ahmedrafat845 <za399012@gmail.com>
2024-09-30 17:22:01 +0300

    l

--- DataBase/dbConnection.js
+++ DataBase/dbConnection.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 export const DbConnection = () => {
     console.log('Connecting to MongoDB with URI:', process.env.MONGODB_URI); // Log the URI
     mongoose.connect(process.env.MONGODB_URI)
