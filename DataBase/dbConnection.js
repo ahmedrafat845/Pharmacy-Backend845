2b0e6754b35b2045f17e2b8697d32f9b2eb03712
ahmedrafat845 <za399012@gmail.com>
2024-09-30 16:40:30 +0300

    ahmed1

--- DataBase/dbConnection.js
+++ DataBase/dbConnection.js
@@ -1,10 +1,11 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-
-export const DbConnection=()=>{
-    mongoose.connect('mongodb+srv://ahmedrafat12:ahmedrafat12345@cluster0.mnfstos.mongodb.net/Pharmacy').then(()=>{
-        console.log(' dataBase Connected111')
-    }).catch((error)=>{
-        console.log('error in dataBase');     
-    })
-}
+export const DbConnection = () => {
+    mongoose.connect(process.env.MONGODB_URI)
+        .then(() => {
+            console.log('Database Connected Successfully');
+        })
+        .catch((error) => {
+            console.error('Error connecting to the database:', error.message);
+        });
+};
--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-
-import { DbConnection } from './DataBase/dbConnection.js'
-import userRouter from './src/modules/user/user.router.js'
-import { productRouter } from './src/modules/product/product.router.js'
-import { cartRouter } from './src/modules/cart/cart.router.js'
-import { wishlistRouter } from './src/modules/wishlist/wishlist.router.js'
+import { DbConnection } from './DataBase/dbConnection.js';
+import userRouter from './src/modules/user/user.router.js';
+import { productRouter } from './src/modules/product/product.router.js';
+import { cartRouter } from './src/modules/cart/cart.router.js';
+import { wishlistRouter } from './src/modules/wishlist/wishlist.router.js';
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import paymentRouter from './src/modules/payment/payment.router.js';
 
 dotenv.config();
-const app = express()
-DbConnection()
-const port = 3000
+const app = express();
+DbConnection();
+
+const port = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
-app.use('/users',userRouter)
-app.use('/products',productRouter)
-app.use('/carts',cartRouter)
-app.use('/wishlist',wishlistRouter)
-
+app.use('/users', userRouter);
+app.use('/products', productRouter);
+app.use('/carts', cartRouter);
+app.use('/wishlist', wishlistRouter);
 app.use('/payment', paymentRouter);
+
 app.get('/', (req, res) => {
     res.send('Welcome to the API');
-  });
-  
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
