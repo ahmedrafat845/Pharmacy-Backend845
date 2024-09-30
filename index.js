b46a3ac4f7560019baf7ca0c8dbc1abdd167bbf9
ahmedrafat845 <za399012@gmail.com>
2024-09-30 17:06:17 +0300

    donia

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 import { DbConnection } from './DataBase/dbConnection.js';
 import userRouter from './src/modules/user/user.router.js';
 import { productRouter } from './src/modules/product/product.router.js';
 import { cartRouter } from './src/modules/cart/cart.router.js';
 import { wishlistRouter } from './src/modules/wishlist/wishlist.router.js';
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
 import paymentRouter from './src/modules/payment/payment.router.js';
 
 dotenv.config();
 const app = express();
-DbConnection();
-
 const port = process.env.PORT || 3000;
 
+// Database connection
+DbConnection();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
 app.use('/users', userRouter);
 app.use('/products', productRouter);
 app.use('/carts', cartRouter);
@@ -26,4 +30,11 @@ app.get('/', (req, res) => {
     res.send('Welcome to the API');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Error handling middleware (optional)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start the server
+app.listen(port, () => console.log(`Server is running on port ${port}!`));
