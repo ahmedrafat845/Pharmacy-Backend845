d817b2e8f67774accce2e6ccc069b798a638bfd0
ahmedrafat845 <za399012@gmail.com>
2024-10-09 23:06:04 +0300

    url

--- DataBase/dbConnection.js
+++ DataBase/dbConnection.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 export const DbConnection = () => {
-    console.log('Connecting to MongoDB with URI:', process.env.MONGODB_URI); // Log the URI
-    mongoose.connect(process.env.MONGODB_URI)
-        .then(() => {
-            console.log('Database Connected Successfully');
-        })
-        .catch((error) => {
-            console.error('Error connecting to the database:', error.message);
-        });
+    const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://ahmedrafat12:ahmedrafat12345@cluster0.mnfstos.mongodb.net/Pharmacy';
+    console.log('Connecting to MongoDB with URI:', mongoUri); // Log the URI for debugging
+    
+    mongoose.connect(mongoUri)
+    .then(() => {
+        console.log('Database Connected Successfully');
+    })
+    .catch((error) => {
+        console.error('Error connecting to the database:', error.message);
+    });
 };
